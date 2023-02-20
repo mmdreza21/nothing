067a6da7a2ba3d6e27fa1775fa3e7ee5a039dfbc
mrezaj1377 <mrezaj1377@gmail.com>
2023-02-20 07:50:03 +0330

    link list starting

--- Arrays/Program.cs
+++ Arrays/Program.cs
@@ -10,16 +10,16 @@ namespace DataStructures
     {
         static void Main(string[] args)
         {
-            Arrays<int> arr = new Arrays<int>(2);
+            LinkedList<int> linklist = new LinkedList<int>();
+            linklist.AddLast(3);
+            linklist.AddFirst(5);
 
-            arr.Insert(2);
-            arr.Insert(3);
-            arr.Insert(5);
-            arr.Insert(6);
-            arr.Insert(8);
-            arr.Remove(9);
-            Console.WriteLine(arr.IndexOf(3));
-            arr.Print();
+            Console.WriteLine(linklist);
+            foreach (var i in linklist)
+            {
+                Console.WriteLine(i);
+            }
+
         }
     }
 }
